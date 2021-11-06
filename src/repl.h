d0efc01f2357dfa48f50e5892b18b80451731faa
PoorlyDefinedBehaviour <brunotj2015@hotmail.com>
2021-11-06 17:49:50 -0300

    fix(repl): remove new line added automatically by fgets

--- src/repl.h
+++ src/repl.h
@@ -1,3 +1,4 @@
+#include <string.h>
 #include "./vm.h"
 
 static void repl(Vm *vm)
@@ -12,6 +13,13 @@ static void repl(Vm *vm)
     {
       printf("\n");
       break;
+    }
+
+    int line_length = strlen(line);
+
+    if (line[line_length - 1] == '\n')
+    {
+      line[line_length - 1] = '\0';
     }
 
     interpret(vm, line);
